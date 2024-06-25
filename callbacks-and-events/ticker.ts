a9f751d77a08c518cb7a81a1af25e8846b2f1662
TALLIY <mohammadaltalli@gmail.com>
2024-06-25 20:01:21 +0200

    errors

--- callbacks-and-events/ticker.ts
+++ callbacks-and-events/ticker.ts
@@ -2,27 +2,43 @@ import { EventEmitter } from "stream";
 
 const ticker = (
   num_ms: number,
-  callback: (n: number) => void
+  total_num_ms: number,
+  callback: (err: Error | null, n?: number) => void
 ): EventEmitter => {
+  const t_0 = Date.now();
   let ticks = 0;
   const eventEmitter = new EventEmitter();
-  process.nextTick(() => eventEmitter.emit("tick", ticks));
+  process.nextTick(() => eventEmitter.emit("tick", ticks, Date.now()));
 
-  const setTimeout50ms = () => {
+  const timeout = () => {
     setTimeout(() => {
       ticks += 1;
+      if (Date.now() % 5 === 0) {
+        callback(new Error("Error: Timestamp is divisible by 5"));
+        eventEmitter.emit(
+          "error",
+          new Error("Error: Timestamp is divisible by 5")
+        );
+      }
       eventEmitter.emit("tick", ticks);
-      ticks * 50 < num_ms ? setTimeout50ms() : callback(ticks);
-    });
+      const t_1 = Date.now();
+      t_1 - t_0 < total_num_ms ? timeout() : callback(null, ticks);
+    }, num_ms);
   };
 
-  setTimeout50ms();
+  timeout();
 
   return eventEmitter;
 };
 
-ticker(350, (ticks: number) => {
-  console.log("final tick count: ", ticks);
-}).on("tick", (ticks) => {
-  console.log("current tick count: ", ticks);
-});
+ticker(50, 350, (err: Error | null, ticks?: number) => {
+  if (err) {
+    console.log("Error: ", err);
+  } else {
+    console.log("final tick count: ", ticks);
+  }
+})
+  .on("tick", (ticks?: number) => {
+    console.log("current tick count: ", ticks);
+  })
+  .on("error", (err) => console.log("Error emitted: ", err));
